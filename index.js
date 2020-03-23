14ec13e25bb26c2c5e98cfd230eeca9f0f83e9b7
Nailya Ismayilova <nailya.ismail@gmail.com>
2020-03-23 16:07:27 +0400

    test

--- index.js
+++ index.js
@@ -1,41 +1,40 @@
 document.getElementById("button-count").addEventListener('click', calcBankDeposit);
+//document.getElementById('balance');
+
 function calcBankDeposit() {
     let inputamount = +document.getElementById('initialamount').value;
     let inputtopupamount = +document.getElementById('topupamount').value;
     let inputpercent = +document.getElementById('percent').value;
     let inputdate = +document.getElementById('date').value;
     let finalresult = inputamount;
+    
      
     if (inputamount <= 0 || isNaN(inputamount)){
-        showMistake();
-        console.log('mistake');
+        showMistake('mistake');
         return NaN;
-
     }else if (inputtopupamount <= 0 || isNaN(inputamount)) {
-        showMistake();
-        console.log('mistake');
+        showMistake('mistake');
         return NaN;
     }else if (inputpercent < 1 || inputpercent > 100) {
-        showMistake();
-        console.log('mistake');
+        showMistake('mistake');
         return NaN;
     }else if (inputdate <= 0 || isNaN(inputdate)) {
-        showMistake();
-        console.log('mistake'); 
+        showMistake('mistake'); 
         return NaN;
     } else {
+         showMistake('');
         let month = Math.floor(inputdate / 30);
         for (let i = 0; i < month; i++) {
-          finalresult = finalresult + inputtopupamount + (finalresult + inputtopupamount) * inputpercent / 1200;
+          finalresult = finalresult + inputtopupamount + finalresult * inputpercent / 1200;
         }
-        alert(Math.ceil(finalresult));
+        
+         document.getElementById('result').innerHTML =finalresult;
+         //alert(finalresult);
     }
     
 }
 
-function showMistake () {
-    document.getElementById('error').style.display="block";
+function showMistake (text) {
+    document.getElementById('result').innerHTML=text;
 }
-
-
 
